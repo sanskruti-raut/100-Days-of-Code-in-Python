643a1b5abbbb92d7c2067a317dbdc38bf4558454
Sanskruti Raut <sanskrutiraut14@gmail.com>
2025-11-09 14:53:18 -0800

    day1

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator!")
+print("Let's create a band name for you.")
+name = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+band_name = name + " " + pet
+print("Your band name could be: " + band_name)
