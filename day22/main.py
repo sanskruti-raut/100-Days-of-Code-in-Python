ef3ac9bb65deebe85246cb10b1dd418541de4ab0
Sanskruti Raut <sanskrutiraut14@gmail.com>
2025-12-08 23:38:13 -0800

    day23

---
+++ day22/main.py
@@ -0,0 +1,36 @@
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.setup(width=600, height=600)
+screen.exitonclick()
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
+
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
